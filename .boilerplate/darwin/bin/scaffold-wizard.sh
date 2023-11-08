d99562352b2201d72a2bc96a9811cb9718db974b
stuart_zhang <stuartpage@qq.com>
2023-11-08 09:59:57 +0800

    这一版先完全依赖于：对 gtk+3 链接库的 brew install 预安装了。

--- .boilerplate/darwin/bin/scaffold-wizard.sh
+++ .boilerplate/darwin/bin/scaffold-wizard.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-WORK_DIR=$(dirname $0)
-DYLD_LIBRARY_PATH=$WORK_DIR/../lib:$DYLD_LIBRARY_PATH $WORK_DIR/scaffold-wizard
+brew list pkg-config || sudo brew install --prefix=/usr/local pkg-config
+brew list gtk+3 || sudo brew install --prefix=/usr/local gtk+3
+$(dirname $0)/scaffold-wizard
