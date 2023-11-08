c8764805f3b4a0652b73ddfb53837ee599a07bb2
stuart_zhang <stuartpage@qq.com>
2023-11-08 11:32:51 +0800

    修改安装指令

--- .boilerplate/darwin/bin/scaffold-wizard.sh
+++ .boilerplate/darwin/bin/scaffold-wizard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-brew list pkg-config || sudo brew install --prefix=/usr/local pkg-config
-brew list gtk+3 || sudo brew install --prefix=/usr/local gtk+3
+brew list pkg-config || sudo brew install pkg-config
+brew list gtk+3      || sudo brew install gtk+3
 $(dirname $0)/scaffold-wizard
